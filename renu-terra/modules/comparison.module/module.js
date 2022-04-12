44579dc54463573677dd352d122ccf3aa719b721
Brandon Phillips <brandon@fullmetalworkshop.com>
2022-04-12 11:57:24 -0400

    initial commit

---
+++ renu-terra/modules/comparison.module/module.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  $('.renuble-comparison').slick({
+  });
+});
---
+++ renu-terra/modules/testimonials.module/module.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('.renuble-testimonials').slick({
+    arrows: true
+  });
+});
